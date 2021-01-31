dce75f8fa05e9fed1b4b61b6c96ddf00828e8e13
Shadi497 <b.shadi.h@gmail.com>
2021-01-31 09:49:42 +0300

    Data added

---
+++ src/Data.js
@@ -0,0 +1,36 @@
+const trips = [
+  {
+    id: 1,
+    name: "Escalante Route",
+    image:
+      "https://www.reserveamerica.com/marketing/html/acm/__shared/assets/escalante_route2493.jpg",
+    city: "Arizona",
+    difficulty: "Hard",
+    length: "33-mile",
+    rating: "5 stars",
+  },
+
+  {
+    id: 2,
+    name: "Hut to Hut Presidential Traverse",
+    image:
+      "https://www.reserveamerica.com/marketing/html/acm/__shared/assets/hut_to_hut2494.jpg",
+    city: "New Hampshire",
+    difficulty: "Medium",
+    length: "25.5 miles",
+    rating: "4 stars",
+  },
+
+  {
+    id: 3,
+    name: "Teton Crest Trail",
+    image:
+      "https://www.reserveamerica.com/marketing/html/acm/__shared/assets/teton_crest_trail2498.jpg",
+    city: "Wyoming",
+    difficulty: "Easy",
+    length: "40 miles",
+    rating: "3 stars",
+  },
+];
+
+export default trips;
